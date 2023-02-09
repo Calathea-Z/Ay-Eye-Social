49be825a3fceaf65204734d28e048200eb0e1a33
Calathea-Z <zsykes21@gmail.com>
2023-02-09 13:25:54 -0500

    Begin work building out home page, added in loader animation.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
+import Card from "../components/Card";
+import FormField from "../components/FormField"
+import { GridLoader } from "react-spinners";
+import { useState, useEffect } from 'react'
+
 
 const Home = () => {
+    const [loading, setLoading] = useState(false);
+    const [allPosts, setAllPosts] = useState(null);
     return (
-        <h1>Home</h1>
+        <section className='max-w-7xl mx-auto'>
+            <div>
+                <h1 className='font-extrabold text-[#222328] text-[32px]'>
+                    Community Showcase
+                </h1>
+                <p className='mt-2 text-[#e66675] text-[14px] max-w'>
+                    Browse through a collection of AI generated images
+                </p>
+            </div>
+
+            <div className='mt-16'>
+                <FormField />
+            </div>
+
+            <div className='mt-10'>
+                {loading? (
+                    <div className='flex justify-center items-center'>
+                        <GridLoader color={aqua}/>
+                    </div>
+                ) : (<> </>)}
+            </div>
+        </section>
     )
 }
 
